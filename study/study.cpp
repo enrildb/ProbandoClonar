eb8ccef6a0fbfda3b48ab3e01d24a19f5e6aab02
enrildb <82163298+enrildb@users.noreply.github.com>
2021-05-12 16:42:01 +0200

    Add files via upload

---
+++ study/study.cpp
@@ -0,0 +1,16 @@
+// study.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	string v;
+	for (int i = 0; i < 4; i++) {
+		cin >> v[i];
+	}
+	cout << v << endl;
+}
+
